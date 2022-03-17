b37ceffc060c687b69d642c7f17327dd6a6707ef
BlsnA <99727349+BlsnA@users.noreply.github.com>
2022-03-17 19:19:32 +0100

    Update password-generator-manager.py

--- password-generator-manager.py
+++ password-generator-manager.py
@@ -24,17 +24,17 @@ def view():
 		for line in file.readlines():
 			line = line.rstrip()
 			username, password = line.split("|")
-			print('User:', username, '|', 'Password:', fer.decrypt(password.encode()).decode())
+			print("User:", username, "|", "Password:", fer.decrypt(password.encode()).decode())
 
 
 def generator():
 	chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!§$%&/()=?€0123456789'
-	length = int(input('Input the length of your new password: '))
+	length = int(input("Input the length of your new password: "))
 	password_generated = ''
 	for c in range(length):
 		password_generated += random.choice(chars)
-	print('This is the generated password: ', password_generated)
-	yes_no = input('Would you like to use this as your password?' "\n" \
+	print("This is the generated password: ", password_generated)
+	yes_no = input("Would you like to use this as your password?" "\n" \
 		'Enter \'yes\' to add it to your file' "\n" \
 		'Enter \'no\' to exit the program' "\n" )
 	if yes_no.lower() == 'yes':
@@ -43,15 +43,15 @@ def generator():
 		quit()
 
 def add():
-	username_new = input('Account Name: ')
+	username_new = input("Account Name: ")
 	password_new = generator()
 	with open('passwords.txt', 'a') as file:
 		file.write(username_new + "|" + fer.encrypt(password_new.encode()).decode() + "\n")
-	print('The new password has been added to your file.')
+	print("The new password has been added to your file.")
 
 
 while True: 
-	mode = input('Would you like to a new passwords or view existing ones?' + "\n" \
+	mode = input("Would you like to a new passwords or view existing ones?" + "\n" \
 		'Enter \'view\' to view existing passwords' "\n" \
 		'Enter \'add\' to add a new password to your list' "\n" \
 		'Enter \'quit\' to exit the program' "\n")
@@ -63,5 +63,5 @@ while True:
 	elif mode == 'add':
 		add()
 	else:
-		print('Invalid input')
+		print("Invalid input")
 		continue
